a603c61bf2d0d6c119b7ee7c894c909100d73730
sabarishravi93 <81555633+sabarishravi93@users.noreply.github.com>
2025-07-02 16:51:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "circleci-node-terraform-state-1"   # Replace with your S3 bucket name
+    bucket         = "circleci-node-terraform-state-2"   # Replace with your S3 bucket name
     key            = "terraform/terraform.tfstate"      # Path within the bucket
     region         = "us-east-1"                        # Same region where EC2 is launched
-    dynamodb_table = "circleci-node-terraform-locks-1"    # Replace with your DynamoDB table (for locking)
+    dynamodb_table = "circleci-node-terraform-locks-2"    # Replace with your DynamoDB table (for locking)
     encrypt        = true
   }
 }
